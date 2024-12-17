d5c20516074bba9ada2c294d517e120916600592
DarkNjm <njmmadhamm@gmail.com>
2024-12-17 17:19:46 +0300

    Update config.py
    Signed-off-by: DarkNjm <njmmadhamm@gmail.com>

--- config.py
+++ config.py
@@ -5,23 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID"))  # معرف API الخاص بك
+API_HASH = getenv("API_HASH")  # API HASH الخاص بك
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+BOT_TOKEN = getenv("BOT_TOKEN", None)  # رمز البوت الخاص بك
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))  # الحد الأقصى للمدة (بالثواني)
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID"))  # معرف مالك البوت
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")  # صورة ال Ping الافتراضية
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")  # صورة البدء الافتراضية
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("SESSION", None)  # سلسلة الجلسة الخاصة بك
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")  # رابط دردشة الدعم
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")  # قناة الدعم
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))  # قائمة المستخدمين المسؤولين (sudo)
 
-
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"  # صورة فشل العملية
