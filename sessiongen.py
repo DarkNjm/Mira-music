d9a81b1e70f6d1b3c659c2715e638dec5d3e43f9
DarkNjm <njmmadhamm@gmail.com>
2024-12-17 17:21:11 +0300

    Update sessiongen.py
    Signed-off-by: DarkNjm <njmmadhamm@gmail.com>

--- sessiongen.py
+++ sessiongen.py
@@ -3,10 +3,10 @@ import asyncio
 from pyrogram import Client, __version__ as ver
 
 
-API_ID = input("\nEnter Your API ID :\n > ")
-API_HASH = input("\nEnter Your API HASH :\n > ")
+API_ID = input("\nأدخل API ID الخاص بك :\n > ")
+API_HASH = input("\nأدخل API HASH الخاص بك :\n > ")
 
-print("\n\nEnter the phone number associated with your telegram account when asked.\n\n")
+print("\n\nأدخل رقم الهاتف المرتبط بحسابك على تيليجرام عندما يُطلب منك.\n\n")
 
 fallen = Client("Fallen", api_id=API_ID, api_hash=API_HASH, in_memory=True)
 
@@ -14,9 +14,9 @@ fallen = Client("Fallen", api_id=API_ID, api_hash=API_HASH, in_memory=True)
 async def main():
     await fallen.start()
     sess = await fallen.export_session_string()
-    txt = f"Here is your Pyrogram {ver} String Session\n\n<code>{sess}</code>\n\nDon't share it with anyone.\nDon't forget to join @FallenAssociation"
+    txt = f"إليك سلسلة الجلسة الخاصة بك باستخدام Pyrogram {ver}\n\n<code>{sess}</code>\n\nلا تشاركها مع أي شخص.\nلا تنسى الانضمام إلى @FallenAssociation"
     ok = await fallen.send_message("me", txt)
-    print(f"Here is your Pyrogram {ver} String Session\n{sess}\nDouble click to copy.") 
+    print(f"إليك سلسلة الجلسة الخاصة بك باستخدام Pyrogram {ver}\n{sess}\nانقر مرتين لنسخها.") 
 
 
 asyncio.run(main())
